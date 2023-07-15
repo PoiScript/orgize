dca473a08b1fbd8011e195cb79931dd76eb9d878
Justin Abrahms <justin@abrah.ms>
2023-07-14 17:26:43 -0700

    I don't think we actually care about default values here.

--- src/export/mod.rs
+++ src/export/mod.rs
@@ -12,7 +12,7 @@ use std::io::{Error, Write};
 
 use crate::elements::{Datetime, Element};
 
-pub trait ExportHandler<E: From<Error>>: Default {
+pub trait ExportHandler<E: From<Error>> {
     fn start<W: Write>(&mut self, writer: W, element: &Element) -> Result<(), E>;
     fn end<W: Write>(&mut self, writer: W, element: &Element) -> Result<(), E>;
 }
